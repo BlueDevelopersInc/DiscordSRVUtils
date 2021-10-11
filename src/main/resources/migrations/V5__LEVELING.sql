e4ae924eab5d0854a3ef09e6de3f5e3c61cd7a82
BlueTree242 <69328929+BlueTree242@users.noreply.github.com>
2021-10-11 20:49:16 +0200

    Decreased leveling uuid length

--- src/main/resources/migrations/V5__LEVELING.sql
+++ src/main/resources/migrations/V5__LEVELING.sql
@@ -20,4 +20,4 @@
  *  END
  */
 
-CREATE TABLE leveling(UUID varchar(1000) PRIMARY KEY , Name varchar(50), Level int, XP int, DiscordMessages int, MinecraftMessages int)
+CREATE TABLE leveling(UUID varchar(50) PRIMARY KEY , Name varchar(50), Level int, XP int, DiscordMessages int, MinecraftMessages int)
