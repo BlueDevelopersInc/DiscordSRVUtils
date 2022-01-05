2375e7ca1619582b830f9577c7a294527ec414ad
BlueTree242 <yahiamsm2@gmail.com>
2022-01-05 15:36:42 +0200

    readd leveling-roles.json to debug report removed by mistake

--- src/main/java/tk/bluetree242/discordsrvutils/utils/DebugUtil.java
+++ src/main/java/tk/bluetree242/discordsrvutils/utils/DebugUtil.java
@@ -156,7 +156,7 @@ public class DebugUtil {
         files.add(fileMap("leveling.yml", Utils.readFile(core.getBukkitMain().getDataFolder() + core.fileseparator + "leveling.yml")));
         files.add(fileMap("status.yml", Utils.readFile(core.getBukkitMain().getDataFolder() + core.fileseparator + "status.yml")));
         files.add(fileMap("suggestions.yml", Utils.readFile(core.getBukkitMain().getDataFolder() + core.fileseparator + "suggestions.yml")));        files.add(fileMap("leveling-roles.json", Utils.readFile(core.getBukkitMain().getDataFolder() + core.fileseparator + "leveling-roles.json")));
-
+        files.add(fileMap("leveling-roles.json", Utils.readFile(core.getBukkitMain().getDataFolder() + core.fileseparator + "leveling-roles.json")));
         return files;
     }
 
