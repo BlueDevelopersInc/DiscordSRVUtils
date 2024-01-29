9860a63db59907429103f7de082a61eeaf898c07
BlueTree242 <yahiamsm2@gmail.com>
2024-01-29 13:31:41 +0200

    Remove lines between spaces in sql.yml

--- core/src/main/java/dev/bluetree242/discordsrvutils/config/SQLConfig.java
+++ core/src/main/java/dev/bluetree242/discordsrvutils/config/SQLConfig.java
@@ -37,27 +37,27 @@ public interface SQLConfig {
     boolean isEnabled();
 
     @ConfDefault.DefaultString("localhost")
-    @ConfComments("\n# Host for your database, usually localhost.")
+    @ConfComments("# Host for your database, usually localhost.")
     @AnnotationBasedSorter.Order(2)
     String Host();
 
     @ConfDefault.DefaultInteger(3306)
-    @ConfComments("\n# Port for your Database, usually 3306.")
+    @ConfComments("# Port for your Database, usually 3306.")
     @AnnotationBasedSorter.Order(3)
     int Port();
 
     @ConfDefault.DefaultString("root")
-    @ConfComments("\n# Username used to login to database.")
+    @ConfComments("# Username used to login to database.")
     @AnnotationBasedSorter.Order(4)
     String UserName();
 
     @ConfDefault.DefaultString("password")
-    @ConfComments("\n# Password used to login to database.")
+    @ConfComments("# Password used to login to database.")
     @AnnotationBasedSorter.Order(5)
     String Password();
 
     @ConfDefault.DefaultString("DiscordSRVUtilsData")
-    @ConfComments("\n# Database name. The host should tell you the name normally.")
+    @ConfComments("# Database name. The host should tell you the name normally.")
     @AnnotationBasedSorter.Order(6)
     String DatabaseName();
 }
