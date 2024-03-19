f59abdea18555a6cdd09c2cbd4d09ef0a3acd98d
BlueTree242 <yahiamsm2@gmail.com>
2024-03-19 19:57:11 +0200

    Re-add /dsu removeslash

--- core/src/main/java/dev/bluetree242/discordsrvutils/commands/game/DiscordSRVUtilsCommand.java
+++ core/src/main/java/dev/bluetree242/discordsrvutils/commands/game/DiscordSRVUtilsCommand.java
@@ -57,142 +57,145 @@ public class DiscordSRVUtilsCommand implements PlatformCommand {
     public void onRunAsync(String[] args, CommandUser sender, String label) throws Throwable {
         if (args.length == 0) {
             sender.sendMessage("&eRunning DiscordSRVUtils v&a" + core.getPlatform().getDescription().getVersion());
+            //noinspection ConstantValue
             sender.sendMessage("&eBuild " + (VersionInfo.BUILD_NUMBER.equalsIgnoreCase("NONE") ? "&aNone/Unknown" : "&a#" + VersionInfo.BUILD_NUMBER));
             sender.sendMessage("&bStatus: " + (core.isReady() ? "&aRunning and functioning" : "&cNot running"));
             return;
         }
-        if (args.length >= 1) {
-            if (args[0].equalsIgnoreCase("reload")) {
-                if (sender.hasPermission("discordsrvutils.reload")) {
-                    sender.sendMessage("&eReloading Configuration");
-                    try {
-                        core.reloadConfigs();
-                        sender.sendMessage("&aConfiguration successfully reloaded");
-                    } catch (ConfigurationLoadException e) {
-                        sender.sendMessage("&cCould not reload the " + e.getConfigName() + ". Please check server console");
-                        e.printStackTrace();
+        if (args[0].equalsIgnoreCase("reload")) {
+            if (sender.hasPermission("discordsrvutils.reload")) {
+                sender.sendMessage("&eReloading Configuration");
+                try {
+                    core.reloadConfigs();
+                    sender.sendMessage("&aConfiguration successfully reloaded");
+                } catch (ConfigurationLoadException e) {
+                    sender.sendMessage("&cCould not reload the " + e.getConfigName() + ". Please check server console");
+                    e.printStackTrace();
+                }
+                return;
+            }
+        } else if (args[0].equalsIgnoreCase("debug")) {
+            if (sender.hasPermission("discordsrvutils.debug")) {
+                sender.sendMessage("&aPreparing Debug Report... Please wait");
+                try {
+                    sender.sendMessage("&aYour Debug report is available at: &e" + core.getServer().getDebugger().run());
+                } catch (Exception e) {
+                    sender.sendMessage("&cERROR: " + e.getMessage());
+                }
+                return;
+            }
+        } else if (args[0].equalsIgnoreCase("updatecheck")) {
+            if (sender.hasPermission("discordsrvutils.updatecheck")) {
+                try {
+                    UpdateChecker.UpdateCheckResult result = core.getUpdateChecker().updateCheck(false);
+                    if (result == null) sender.sendMessage("&cLooks like update checking is disabled.");
+                    else {
+                        if (result.getMessage() == null) {
+                            if (result.getVersionsBehind() <= 0) {
+                                sender.sendMessage("&aYou are up to date.");
+                            } else {
+                                sender.sendMessage(Component.text("You are ").color(NamedTextColor.YELLOW)
+                                        .append(Component.text(result.getVersionsBehind()).color(NamedTextColor.RED))
+                                        .appendSpace().append(Component.text("versions behind. Update is available at "))
+                                        .append(Component.text(result.getDownloadUrl()).color(NamedTextColor.AQUA).clickEvent(ClickEvent.openUrl(result.getDownloadUrl())))
+                                );
+                            }
+                        } else sender.sendMessage(result.getMessage());
                     }
+                } catch (InvalidResponseException e) {
+                    sender.sendMessage("&4Failed to check for updates: &c" + e.getFriendlyMessage());
+                }
+                return;
+            }
+        } else if (args[0].equalsIgnoreCase("resetlevel")) {
+            if (sender.hasPermission("discordsrvutils.resetlevel")) {
+                String name = args.length >= 2 ? args[1] : null;
+                if (name == null) {
+                    sender.sendMessage("&cPlease provide player name or all for all players.");
                     return;
                 }
-            } else if (args[0].equalsIgnoreCase("debug")) {
-                if (sender.hasPermission("discordsrvutils.debug")) {
-                    sender.sendMessage("&aPreparing Debug Report... Please wait");
-                    try {
-                        sender.sendMessage("&aYour Debug report is available at: &e" + core.getServer().getDebugger().run());
-                    } catch (Exception e) {
-                        sender.sendMessage("&cERROR: " + e.getMessage());
-                    }
-                    return;
-                }
-            } else if (args[0].equalsIgnoreCase("updatecheck")) {
-                if (sender.hasPermission("discordsrvutils.updatecheck")) {
-                    try {
-                        UpdateChecker.UpdateCheckResult result = core.getUpdateChecker().updateCheck(false);
-                        if (result == null) sender.sendMessage("&cLooks like update checking is disabled.");
-                        else {
-                            if (result.getMessage() == null) {
-                                if (result.getVersionsBehind() <= 0) {
-                                    sender.sendMessage("&aYou are up to date.");
-                                } else {
-                                    sender.sendMessage(Component.text("You are ").color(NamedTextColor.YELLOW)
-                                            .append(Component.text(result.getVersionsBehind()).color(NamedTextColor.RED))
-                                            .appendSpace().append(Component.text("versions behind. Update is available at "))
-                                            .append(Component.text(result.getDownloadUrl()).color(NamedTextColor.AQUA).clickEvent(ClickEvent.openUrl(result.getDownloadUrl())))
-                                    );
-                                }
-                            } else sender.sendMessage(result.getMessage());
-                        }
-                    } catch (InvalidResponseException e) {
-                        sender.sendMessage("&4Failed to check for updates: &c" + e.getFriendlyMessage());
-                    }
-                    return;
-                }
-            } else if (args[0].equalsIgnoreCase("resetlevel")) {
-                if (sender.hasPermission("discordsrvutils.resetlevel")) {
-                    String name = args.length >= 2 ? args[1] : null;
-                    if (name == null) {
-                        sender.sendMessage("&cPlease provide player name or all for all players.");
-                        return;
-                    }
-                    if (name.equalsIgnoreCase("all")) {
-                        core.getLevelingManager().resetLeveling();
-                        core.getLevelingManager().cachedUUIDS.invalidateAll();
-                        core.getLevelingManager().getLevelingRewardsManager().setRewardCache(Utils.OBJECT_MAPPER.createObjectNode());
-                        core.getLevelingManager().getLevelingRewardsManager().saveRewardCache();
-                        sender.sendMessage("&eEveryone's level has been reset");
-                    } else {
-                        PlayerStats stats = core.getLevelingManager().getPlayerStats(name);
-                        if (stats == null) {
-                            sender.sendMessage("&cPlayer not found");
-                        } else {
-                            stats.setLevel(0);
-                            stats.setXP(0);
-                            core.getLevelingManager().getLevelingRewardsManager().getRewardCache().remove(stats.getUuid().toString());
-                            core.getLevelingManager().getLevelingRewardsManager().saveRewardCache();
-                            sender.sendMessage("&ePlayer's level has been reset.");
-                        }
-                    }
-                    return;
-                }
-            } else if (args[0].equalsIgnoreCase("migrateLeveling")) {
-                if (sender instanceof ConsoleCommandUser) { // Only console
-                    if (migrated) {
-                        sender.sendMessage("&cAlready migrated.");
-                        return;
-                    }
-                    migrated = true;
-                    sender.sendMessage("&cMigrating leveling to new mee6 leveling, please wait....");
-                    core.getLevelingManager().convertToMee6();
-                    sender.sendMessage("&eSuccessfully migrated, If you used leveling roles before, please reconfigure according to the new leveling system, keep in mind that leveling roles was upgraded to leveling-&lrewards&e.json (https://wiki.discordsrvutils.xyz/leveling-conversion)");
-                    return;
-                }
-            } else if (args[0].equalsIgnoreCase("addxp") && core.getLevelingConfig().enabled()) {
-                if (sender.hasPermission("discordsrvutils.addxp")) {
-                    String name = args.length >= 2 ? args[1] : null;
-                    if (name == null) {
-                        sender.sendMessage("&cPlease provide player name.");
-                        return;
-                    }
-                    String amountString = args.length >= 3 ? args[2] : null;
-                    if (amountString == null || !Utils.isInt(amountString)) {
-                        sender.sendMessage("&cPlease provide a valid xp amount.");
-                        return;
-                    }
+                if (name.equalsIgnoreCase("all")) {
+                    core.getLevelingManager().resetLeveling();
+                    core.getLevelingManager().cachedUUIDS.invalidateAll();
+                    core.getLevelingManager().getLevelingRewardsManager().setRewardCache(Utils.OBJECT_MAPPER.createObjectNode());
+                    core.getLevelingManager().getLevelingRewardsManager().saveRewardCache();
+                    sender.sendMessage("&eEveryone's level has been reset");
+                } else {
                     PlayerStats stats = core.getLevelingManager().getPlayerStats(name);
                     if (stats == null) {
                         sender.sendMessage("&cPlayer not found");
                     } else {
-                        int xp = Integer.parseInt(amountString);
-                        stats.setXP(xp + stats.getXp(), new LevelupEvent(stats, stats.getUuid()));
-                        sender.sendMessage("&aAdded &6" + xp + " &axp to &6" + name + "&a. Now they are level &6" + stats.getLevel() + " &aand have &6" + stats.getXp() + "&a xp.");
-                        core.getLevelingManager().getLevelingRewardsManager().rewardIfOnline(stats);
-                        return;
+                        stats.setLevel(0);
+                        stats.setXP(0);
+                        core.getLevelingManager().getLevelingRewardsManager().getRewardCache().remove(stats.getUuid().toString());
+                        core.getLevelingManager().getLevelingRewardsManager().saveRewardCache();
+                        sender.sendMessage("&ePlayer's level has been reset.");
                     }
                 }
-            } else if ((args[0].equalsIgnoreCase("sendAfk") || args[0].equalsIgnoreCase("sendUnAfk")) && sender instanceof ConsoleCommandUser) {
+                return;
+            }
+        } else if (args[0].equalsIgnoreCase("migrateLeveling")) {
+            if (sender instanceof ConsoleCommandUser) { // Only console
+                if (migrated) {
+                    sender.sendMessage("&cAlready migrated.");
+                    return;
+                }
+                migrated = true;
+                sender.sendMessage("&cMigrating leveling to new mee6 leveling, please wait....");
+                core.getLevelingManager().convertToMee6();
+                sender.sendMessage("&eSuccessfully migrated, If you used leveling roles before, please reconfigure according to the new leveling system, keep in mind that leveling roles was upgraded to leveling-&lrewards&e.json (https://wiki.discordsrvutils.xyz/leveling-conversion)");
+                return;
+            }
+        } else if (args[0].equalsIgnoreCase("addxp") && core.getLevelingConfig().enabled()) {
+            if (sender.hasPermission("discordsrvutils.addxp")) {
                 String name = args.length >= 2 ? args[1] : null;
                 if (name == null) {
                     sender.sendMessage("&cPlease provide player name.");
                     return;
                 }
-                PlatformPlayer<?> player = core.getServer().getPlayer(name);
-                if (player == null) {
-                    sender.sendMessage("&cPlayer not found.");
+                String amountString = args.length >= 3 ? args[2] : null;
+                if (amountString == null || !Utils.isInt(amountString)) {
+                    sender.sendMessage("&cPlease provide a valid xp amount.");
                     return;
                 }
-                if (!player.shouldSendAfk()) return;
-                if (core.getMainConfig().afk_message_enabled()) {
-                    PlaceholdObjectList holders = new PlaceholdObjectList(core);
-                    holders.add(new PlaceholdObject(core, player, "player"));
-                    TextChannel channel = core.getJdaManager().getChannel(core.getMainConfig().afk_channel());
-                    if (channel == null) {
-                        core.severe("No Channel was found with ID " + core.getMainConfig().afk_channel() + ". Afk/NoLonger message was not sent for " + player.getName());
-                        return;
-                    }
-                    Message msg = core.getMessageManager().getMessage(args[0].equalsIgnoreCase("sendAfk") ? core.getMainConfig().afk_message() : core.getMainConfig().no_longer_afk_message(), holders, player).build();
-                    core.queueMsg(msg, channel).queue();
+                PlayerStats stats = core.getLevelingManager().getPlayerStats(name);
+                if (stats == null) {
+                    sender.sendMessage("&cPlayer not found");
+                } else {
+                    int xp = Integer.parseInt(amountString);
+                    stats.setXP(xp + stats.getXp(), new LevelupEvent(stats, stats.getUuid()));
+                    sender.sendMessage("&aAdded &6" + xp + " &axp to &6" + name + "&a. Now they are level &6" + stats.getLevel() + " &aand have &6" + stats.getXp() + "&a xp.");
+                    core.getLevelingManager().getLevelingRewardsManager().rewardIfOnline(stats);
+                    return;
                 }
             }
+        } else if ((args[0].equalsIgnoreCase("sendAfk") || args[0].equalsIgnoreCase("sendUnAfk")) && sender instanceof ConsoleCommandUser) {
+            String name = args.length >= 2 ? args[1] : null;
+            if (name == null) {
+                sender.sendMessage("&cPlease provide player name.");
+                return;
+            }
+            PlatformPlayer<?> player = core.getServer().getPlayer(name);
+            if (player == null) {
+                sender.sendMessage("&cPlayer not found.");
+                return;
+            }
+            if (!player.shouldSendAfk()) return;
+            if (core.getMainConfig().afk_message_enabled()) {
+                PlaceholdObjectList holders = new PlaceholdObjectList(core);
+                holders.add(new PlaceholdObject(core, player, "player"));
+                TextChannel channel = core.getJdaManager().getChannel(core.getMainConfig().afk_channel());
+                if (channel == null) {
+                    core.severe("No Channel was found with ID " + core.getMainConfig().afk_channel() + ". Afk/NoLonger message was not sent for " + player.getName());
+                    return;
+                }
+                Message msg = core.getMessageManager().getMessage(args[0].equalsIgnoreCase("sendAfk") ? core.getMainConfig().afk_message() : core.getMainConfig().no_longer_afk_message(), holders, player).build();
+                core.queueMsg(msg, channel).queue();
+            }
+        } else if (args[0].equalsIgnoreCase("removeslash") && sender.hasPermission("discordsrvutils.removeslash")) {
+            core.getDiscordSRV().getMainGuild().updateCommands().queue();
+            sender.sendMessage("&aCommands should be removed from the main guild.");
+            return;
         }
         sender.sendMessage("&cSubCommand not found");
     }
